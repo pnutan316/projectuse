9c7c724e0ce302a12cfcab2d19b695690a44a8c8
MSN <MSN@DESKTOP-8Q0NMCD>
2023-12-01 00:47:54 +0530

    Assignment completed

---
+++ src/projectuse/assignment.java
@@ -0,0 +1,115 @@
+package projectuse;
+
+public class assignment {
+	
+	public class Bag {
+		int Size;
+		String colour; 
+		String Material;
+		
+	    void storage() {
+	    }
+	    public class car {
+
+	    	String type;
+	    	String color;
+	    	int petrol;
+	    	Double disel;
+	    	int speed;
+	    	
+	    	void toreachdestination () {
+
+}
+	    	public class chair {
+	    		
+	    		int size;
+	    		String type;
+	    		String color;
+	    		String pattern;
+	    		
+	    		void seating () {
+	    			
+	    			
+	    		}
+	    		public class dog {
+	    			
+	    			String breed;
+	    			   int age;
+	    			   String color;
+
+	    			   void barking() {
+	    			   }
+
+	    			   void hungry() {
+	    			   }
+
+	    			   void sleeping() {
+	    			   }
+	    			}
+	    		public class fan {
+	    			
+	    			String colour;
+	    			int size;
+	    			int speed;
+	    			
+	    			void torotate() {
+	    			
+	    			}
+	    		}
+	    		public class Laptop {
+
+	    			String colour;
+	    			String operations;
+	    			int pricerange;
+	    			String processors;
+	    			String model;
+	    			
+	    			void programmingsetup() {
+	    			
+	    		}
+	    		}
+	    		public class Mobile {
+	    			
+	    			double thickness;
+	    			double processormodel;
+	    			String company;
+	    			double weight;
+	    			
+	    			void entertainment() {
+	    				
+	    				}
+	    		}
+	    		public class tv {
+	    			
+	    			String colour;
+	    			double height;
+	    			double width;
+	    			String brand;
+	    			String modeltype;
+
+	    			void informationaboutentertainment() {
+
+
+	    			}
+	    			}
+	    		public class cat {
+	    			   String breed;
+	    			   int age;
+	    			   String color;
+
+	    			   void barking() {
+	    			   }
+
+	    			   void hungry() {
+	    			   }
+
+	    			   void sleeping() {
+	    			   }
+	    			}
+	    	
+	    	}
+	    }
+	}
+	
+	
+	    	
